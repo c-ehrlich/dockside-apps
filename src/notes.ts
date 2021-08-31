06de049545497bfd4ceb54caebf2865f5ef789f3
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-08-31 15:15:27 +0200

    refactor notes

--- src/notes.ts
+++ src/notes.ts
@@ -5,41 +5,55 @@ var appState = {
   openWindow: "main"
 };
 
-// Immediately invoked function to set visual state (theme, font size) on initial load
-(function () {
-  switch (localStorage.getItem('ds-notes-theme')) {
-    case 'theme-light':
-      setTheme('theme-light');
-      break;
-    case 'theme-dark':
-      setTheme('theme-dark');
-      break;
-    case 'theme-yellow':
-      setTheme('theme-yellow');
-      break;
-    default:
-      setTheme('theme-light');
-  }
-  // textarea.value = localStorage.getItem("text");
+
+// `DOMContentLoaded` may fire before the script has a chance to run, so check before adding a listener
+document.readyState === 'loading' ? document.addEventListener("DOMContentLoaded", init) : init()
+
+
+function init(): void {
+  readLocalStorage()
+  let theme = <string>localStorage.getItem('ds-notes-theme')
+  setTheme(theme);
   (document.querySelector('#txt') as HTMLDivElement).style.fontSize = (localStorage.getItem("ds-notes-font-size") as string)
-})();
+}
 
 
-const textarea = (document.querySelector('#txt') as HTMLTextAreaElement)
-const info = (document.querySelector('#info') as HTMLDivElement)
-const settings = (document.querySelector('#settings') as HTMLDivElement)
+const textarea = <HTMLTextAreaElement>document.querySelector('#txt')
+const info = <HTMLTextAreaElement>document.querySelector('#info')
+const settings = <HTMLTextAreaElement>document.querySelector('#settings')
 textarea.addEventListener('input', writeLocalStorage);
-
-
 document.querySelector('#upper-left')!.addEventListener('click', toggleInfo);
 document.querySelector('#upper-right')!.addEventListener('click', toggleSettings)
 document.querySelector('#upper-middle')!.addEventListener('click', openTextarea)
-
 document.querySelectorAll('.si-fontsize').forEach(item => {
   (item as HTMLDivElement).addEventListener('click', changeFontSize)
 })
+document.querySelectorAll('.settings-item-theme').forEach(item => {
+  (item as HTMLDivElement).addEventListener('click', changeTheme);
+})
 
 
+function writeLocalStorage() {
+  if (typeof(Storage) !== "undefined") {
+      localStorage.setItem("ds-notes-text", textarea.value);
+  } else {
+      document.getElementById("err")!.innerHTML = "Localstorage not supported";
+  }
+}
+
+
+function readLocalStorage() {
+  if (typeof(Storage) !== "undefined") {
+      textarea.value = (localStorage.getItem("ds-notes-text") as string);
+  } else {
+      document.getElementById("err")!.innerHTML = "Localstorage not supported";
+  }
+}
+
+
+/*
+* Helper Functions
+*/
 function changeTheme(e: Event) {
   switch ((e.currentTarget as Element).id) {
     case "select-theme-light":
@@ -69,40 +83,9 @@ function changeFontSize(e: Event) {
 }
 
 
-function writeLocalStorage() {
-  if (typeof(Storage) !== "undefined") {
-      localStorage.setItem("ds-notes-text", textarea.value);
-  } else {
-      document.getElementById("err")!.innerHTML = "Localstorage not supported";
-  }
-}
-
-
-function readLocalStorage() {
-  if (typeof(Storage) !== "undefined") {
-      textarea.value = (localStorage.getItem("ds-notes-text") as string);
-  } else {
-      document.getElementById("err")!.innerHTML = "Localstorage not supported";
-  }
-}
-
-
-// `DOMContentLoaded` may fire before your script has a chance to run, so check before adding a listener
-if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", readLocalStorage);
-} else {  // `DOMContentLoaded` already fired
-    readLocalStorage();
-}
-
-
-
 /*
-* UI Stuff
+* UI Navigation
 */
-document.querySelectorAll('.settings-item-theme').forEach(item => {
-  (item as HTMLDivElement).addEventListener('click', changeTheme);
-})
-
 function toggleInfo() {
   appState.openWindow !== 'info' ? openInfo() : openTextarea()
 }
